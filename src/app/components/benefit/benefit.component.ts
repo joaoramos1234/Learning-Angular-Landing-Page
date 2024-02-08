690e4a25ddbeeafcd16d498c5571269ccb793462
João Ramos <joaoramos13198@gmail.com>
2024-02-08 19:04:28 +0000

    Landing Page
    Landing page made while learning Angular 17.

---
+++ src/app/components/benefit/benefit.component.ts
@@ -0,0 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'benefit',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './benefit.component.html',
+  styleUrl: './benefit.component.scss',
+})
+export class BenefitComponent {}
